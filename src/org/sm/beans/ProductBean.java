f9beda9a175c81f9df4c5c561369a2552742af77
PiscesSarath69 <59145492+Sarath69Kumar@users.noreply.github.com>
2021-11-18 22:49:48 +0530

    Name change
    Name change for clarification

---
+++ src/org/sm/beans/ProductBean.java
@@ -0,0 +1,70 @@
+package org.sm.beans;
+
+public class ProductBean 
+{
+	private String code;
+	private String name;
+	private float price;
+	
+	public ProductBean()
+	{
+		System.out.println("Product");
+	}
+	
+	public ProductBean(String code, String name, float price)
+	{
+		this.code = code;
+		this.name = name;
+		this.price = price;
+		System.out.println("Product with param: code = "+ code + "  name = "+name+ "  price = "+ price );
+	}
+	
+	public String getCode()
+	{
+		return code;
+	}
+	public void setCode(String code)
+	{
+		this.code = code;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public float getPrice()
+	{
+		return price;
+	}
+	public void setPrice(float price)
+	{
+		this.price = price;
+	}
+}
+
+/*
+ * 
+ * 
+-- Create table
+create table PRODUCT
+(
+CODE  VARCHAR(20) not null,
+NAME  VARCHAR(128) not null,
+PRICE FLOAT not null,
+primary key (CODE)
+) ;
+
+
+insert into product (CODE, NAME, PRICE)
+values ('P001', 'Java Core', 100);
+ 
+insert into product (CODE, NAME, PRICE)
+values ('P002', 'C# Core', 90);
+ * 
+ * 
+ */
